4c1b4477b21ad10ecc1ff5fb88f15fec8b8660d4
joepeaden <joe.peaden@gmail.com>
2017-06-28 13:15:50 -0400

    Rename gameController.cs to SimpleShooter/gameController.cs

---
+++ SimpleShooter/gameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameController : MonoBehaviour {
+
+	public int lives;
+	public int score; // For enemies killed, duh
+	public Text livesText; // Text for displaying lives remaining
+	public Text scoreText; // Text displays enemies killed
+
+	private int ammo;
+
+	void Start(){
+
+		lives = 3;
+		score = 0;
+
+		SetStatsText();
+
+	}
+
+	void Update(){
+
+		SetStatsText ();
+
+		SpawnWave ();
+	}
+		
+	void SetStatsText (){// Updates text for player lives and kills
+
+		livesText.text = "Lives Remaining: " + lives.ToString ();
+		scoreText.text = "Enemies Killed: " + score.ToString ();
+		
+	}
+
+	IEnumerator SpawnWave (){
+		yield return new WaitForSeconds(5f);
+	}
+
+}
