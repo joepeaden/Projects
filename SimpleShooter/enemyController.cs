df7053b218f2a954f440da4a59809d34c7328b18
joepeaden <joe.peaden@gmail.com>
2017-06-28 13:13:34 -0400

    Rename enemyController.cs to SimpleShooter/enemyController.cs

---
+++ SimpleShooter/enemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour {
+
+	public float speed;
+	public float jump;
+
+	private Rigidbody2D rb;
+
+	private GameObject player;
+
+	void Start () 
+	{
+		speed = 2;
+		rb = GetComponent<Rigidbody2D>();
+		player = GameObject.Find ("Player");
+	}
+
+	void Update ()
+	{
+		transform.position = Vector3.MoveTowards (transform.position, player.transform.position, speed * Time.deltaTime);
+	}
+
+	void OnTriggerExit2D (Collider2D platformbound)
+	{
+		if (platformbound.tag == "Platformbound") 
+		{
+			Debug.Log ("Platform");
+			if (player.transform.position.y >= transform.position.y) 
+			{
+				rb.velocity = new Vector3 (0, jump, 0);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.name == "Player") 
+			player.GetComponent<playerController> ().isDead = true;
+		}
+}
+
