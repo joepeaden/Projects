f0071c6d09244e54dd40f17fb20667b5e1df9bc0
joepeaden <joe.peaden@gmail.com>
2017-06-28 13:16:03 -0400

    Rename powerScript.cs to SimpleShooter/powerScript.cs

---
+++ SimpleShooter/powerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerScript : MonoBehaviour {
+	
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			// Destroy on contact w/ player
+			// Accessing player game object for weapon alteration
+			col.gameObject.GetComponent<playerController>().weapon = "Lazer";
+			col.gameObject.GetComponent<playerController>().ammo = 100;
+			//GameObject.Find ("GameController").GetComponent<gameController>().lives += 1;
+			Destroy (gameObject);
+		}
+
+	}
+}
