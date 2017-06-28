df0e2c437f40df8fbfc36342ab99216e91993aa6
joepeaden <joe.peaden@gmail.com>
2017-06-28 13:14:41 -0400

    Rename boundaryScript.cs to SimpleShooter/boundaryScript.cs

---
+++ SimpleShooter/boundaryScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boundaryScript : MonoBehaviour {
+
+	private GameObject player;
+
+	void Start (){
+		player = GameObject.Find ("Player");
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject == player)
+			player.GetComponent<playerController> ().isDead = true;
+		else
+			Destroy (other.gameObject);
+	}
+}
