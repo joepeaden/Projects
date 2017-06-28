c1f8dff92cf8df1ee5e3be9ec3a2439d2f0769f1
joepeaden <joe.peaden@gmail.com>
2017-06-28 13:16:16 -0400

    Rename pickupScript.cs to SimpleShooter/pickupScript.cs

---
+++ SimpleShooter/pickupScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupScript : MonoBehaviour {
+
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			// Destroy on contact w/ player
+			// Accessing player game object for weapon alteration
+			col.gameObject.GetComponent<playerController>().weapon = "MachineGun";
+			col.gameObject.GetComponent<playerController>().ammo = 30;
+			//GameObject.Find ("GameController").GetComponent<gameController>().lives += 1;
+			Destroy (gameObject);
+		}
+
+	}
+}
+
